6866b0779bad1430a7ced8b0ee1eb19e8a314919
rufff1 <rufatzg@code.edu.az>
2022-07-11 01:41:03 +0400

    c# task 6

---
+++ Task2/Invoice.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Invoice
+    {
+
+        public string acount;
+        public string customer;
+        public string provider;
+
+        public string Article { get; set; }
+
+        public int Quantity { get; set; }
+
+
+        public Invoice(string acount, string customer, string provider)
+        {
+            this.acount = acount;
+            this.customer = customer;
+            this.provider = provider;
+        }
+
+
+
+        public void CostCalculation(bool NeedEdv)
+        {
+            if (NeedEdv == true)
+            {
+                if (Article == "Usb-hab")
+                {
+                    Console.WriteLine($"{(12 * Quantity) + 12 * 0.18}");
+                    Console.WriteLine($" Article:{Article} Quantity:{Quantity} Price:{12} ");
+                }
+
+
+                else if (Article == "Laptop")
+                {
+                    Console.WriteLine($"Mehsulun deyeri:{(1400 * Quantity) + 1400 * 0.18}");
+                    Console.WriteLine($" Article:{Article} Quantity:{Quantity} Price:{1400}");
+                }
+                else if (Article == "SD-card")
+                {
+                    Console.WriteLine($"Mehsulun deyeri:{(30 * Quantity) + 30 * 0.18}");
+                    Console.WriteLine($"Article:{Article} Quantity:{Quantity} Price:{30}");
+                }
+
+            }
+            else
+            {
+                if (Article == "Usb-hab")
+                {
+                    Console.WriteLine($"Mehsulun umumi deyeri:{12 * Quantity}");
+                    Console.WriteLine($"Article:{Article} Quantity:{Quantity} Price:{12} ");
+                }
+                else if (Article == "Laptop")
+                {
+                    Console.WriteLine($"Mehsulun umumi deyeri:{1400 * Quantity}");
+                    Console.WriteLine($"Article:{Article} Quantity:{Quantity} Price:{1400}");
+                }
+                else if (Article == "SD-card")
+                {
+                    Console.WriteLine($"Mehsulun umumi deyeri{30 * Quantity}");
+                    Console.WriteLine($"Article:{Article} Quantity:{Quantity} Price:{30}");
+                }
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task2
+{
+
+    public class Program
+    {
+
+    
+    static void Main()
+        {
+
+
+            
+
+
+            Invoice inv = new Invoice("678904", "Alex", "Foxtrot")
+            {
+                Article = "Usb-hab",
+                Quantity = 6
+            };
+
+            inv.CostCalculation(false);
+
+
+            Invoice inv2 = new Invoice("678904", "Alex", "Foxtrot")
+            {
+                Article = "Laptop",
+                Quantity = 10
+            };
+           inv2.CostCalculation(true);
+
+
+            Invoice inv3 = new Invoice("678904", "Alex", "Foxtrot")
+            {
+                Article = "SD-card",
+                Quantity = 15
+            };
+         inv3.CostCalculation(true);
+            
+
+
+
+
+        }
+
+
+    }
+}
